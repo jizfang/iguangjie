ccc82d5e02d40409c379c5932133964428279c17
jizfang <ji_zfang@163.com>
2020-06-16 20:31:41 +0800

    项目目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,14 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: '',
+      component: 
+    }
+  ]
 })
-
-export default router
