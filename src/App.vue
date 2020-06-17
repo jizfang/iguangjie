db4afe2b2dad438b19d64ad3c44d4a5882043699
jizfang <ji_zfang@163.com>
2020-06-17 22:42:25 +0800

    首页样式

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,13 @@ export default {
   name: 'App',
   data(){
     return {
-      res:{}
     }
   },
   mounted(){
-    //storage.setItem('a',1);
-    //storage.setItem('user',{a:1});
-    //storage.setItem('abc',{a:1},'user');
-    //storage.clear('a')
-    //storage.clear('a','user')
-
-    this.axios.get('/user/login').then((res) => {
-      this.res = res;
-    })
   }
 }
 </script>
 
 <style>
+  @import './assets/scss/reset.scss';
 </style>
